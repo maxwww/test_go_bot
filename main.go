3871c365df34424c1af8de9613ecc8ee1ce66f8a
maxw <ms.mikro.1@gmail.com>
2019-08-04 23:42:22 +0300

    feat(weather): Implement basic logic of getting weather, add keyboard

--- main.go
+++ main.go
@@ -10,6 +10,18 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
+)
+
+var numericKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Головні новини"),
+		tgbotapi.NewKeyboardButton("Новини Сумщини"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("Погода"),
+		tgbotapi.NewKeyboardButton("Налаштування"),
+	),
 )
 
 func main() {
@@ -33,7 +45,7 @@ func main() {
 		if update.Message == nil {
 			continue
 		}
-		log.Printf("[%s] %s - start", update.Message.From.UserName, update.Message.Text)
+		log.Printf("[%s] %v - start", update.Message.From.UserName, update.Message.Text)
 
 		go func(update tgbotapi.Update) {
 			switch update.Message.Text {
@@ -43,7 +55,7 @@ func main() {
 				if err != nil {
 					log.Fatal(err)
 				}
-			case "/all":
+			case "Головні новини":
 				if response, err := http.Get("https://www.ukr.net/ajax/news.json"); err != nil {
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Something going wrong, try to change your question")
 					_, err := bot.Send(msg)
@@ -73,20 +85,21 @@ func main() {
 						for _, v := range v.Items {
 							message = fmt.Sprintf("%s- %s. [More](%s).\n", message, v.Title, v.URL)
 						}
-						message = message + "\n"
+						message = message + "\n:sunny:"
 					}
 					message = strings.TrimSuffix(message, "\n")
-					message = message + "\nRetry /all or /sumy"
+					message = strings.TrimSuffix(message, "\n")
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, message)
 					msg.ParseMode = "markdown"
 					msg.DisableWebPagePreview = true
+					msg.ReplyMarkup = numericKeyboard
 					_, err := bot.Send(msg)
 					if err != nil {
 						log.Fatal(err)
 					}
 					log.Printf("[%s] %s - sent", update.Message.From.UserName, update.Message.Text)
 				}
-			case "/sumy":
+			case "Новини Сумщини":
 				if response, err := http.Get("https://www.ukr.net/ajax/regions.json?snr=18"); err != nil {
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Something going wrong, try to change your question")
 					_, err := bot.Send(msg)
@@ -113,10 +126,59 @@ func main() {
 					for _, v := range rr.Items {
 						message = fmt.Sprintf("%s- %s. [More](%s).\n", message, v.Title, v.URL)
 					}
-					message = message + "\nRetry /all or /sumy"
+					message = strings.TrimSuffix(message, "\n")
 					msg := tgbotapi.NewMessage(update.Message.Chat.ID, message)
 					msg.ParseMode = "markdown"
 					msg.DisableWebPagePreview = true
+					msg.ReplyMarkup = numericKeyboard
+					_, err := bot.Send(msg)
+					if err != nil {
+						log.Fatal(err)
+					}
+					log.Printf("[%s] %s - sent", update.Message.From.UserName, update.Message.Text)
+				}
+			case "Погода":
+				api_key := os.Getenv("WEATHER_TOKEN")
+				if response, err := http.Get("http://api.openweathermap.org/data/2.5/forecast?q=sumy&units=metric&lang=ua&appid=" + api_key); err != nil {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Something going wrong, try to change your question")
+					_, err := bot.Send(msg)
+					if err != nil {
+						log.Fatal(err)
+					}
+				} else {
+					defer func() {
+						err := response.Body.Close()
+						if err != nil {
+							log.Fatal(err)
+						}
+					}()
+					contents, _ := ioutil.ReadAll(response.Body)
+					WeatherResult := &WeatherForecastResult{}
+					if err = json.Unmarshal([]byte(contents), WeatherResult); err != nil {
+						msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Something going wrong, try to change your question")
+						_, err := bot.Send(msg)
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+					fmt.Println(WeatherResult)
+					message := "*Погода в Сумах*\n"
+					day := ""
+					for _, v := range WeatherResult.List {
+						tm := time.Unix(int64(v.Dt), int64(WeatherResult.City.Timezone))
+						rDay := tm.Format("02 January 2006")
+						if day != rDay {
+							day = rDay
+							message = fmt.Sprintf("%s*%s*\n", message, day)
+						}
+						message = fmt.Sprintf("%s%s *%+d*℃ %s %s %s\n", message, tm.Format("15:04"), int64(v.Main.Temp), Icons[v.Weather[0].Icon], v.Weather[0].Description, GetWindIcon(&v.Wind))
+
+					}
+					message = strings.TrimSuffix(message, "\n")
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, message)
+					msg.ParseMode = "markdown"
+					msg.DisableWebPagePreview = true
+					msg.ReplyMarkup = numericKeyboard
 					_, err := bot.Send(msg)
 					if err != nil {
 						log.Fatal(err)
@@ -124,7 +186,7 @@ func main() {
 					log.Printf("[%s] %s - sent", update.Message.From.UserName, update.Message.Text)
 				}
 			default:
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, Icons["01d"])
 				_, err := bot.Send(msg)
 				if err != nil {
 					log.Fatal(err)
@@ -150,6 +212,38 @@ type RequestResults struct {
 
 type RegionResult struct {
 	Items []Item
+}
+
+type Wind struct {
+	Speed float64 `json:"speed"`
+	Deg   float64 `json:"deg"`
+}
+
+type WeatherForecastResult struct {
+	City struct {
+		Name     string  `json:"name"`
+		Timezone float64 `json:"timezone"`
+	} `json:"city"`
+	List []struct {
+		Dt   float64 `json:"Dt"`
+		Main struct {
+			Temp       float64 `json:"temp"`
+			Temp_min   float64 `json:"temp_min"`
+			Temp_max   float64 `json:"temp_max"`
+			Pressure   float64 `json:"pressure"`
+			Sea_level  float64 `json:"sea_level"`
+			Grnd_level float64 `json:"grnd_level"`
+			Humidity   float64 `json:"humidity"`
+			Temp_kf    float64 `json:"temp_kf"`
+		} `json:"main"`
+		Weather []struct {
+			Id          float64 `json:"id"`
+			Main        string  `json:"main"`
+			Description string  `json:"description"`
+			Icon        string  `json:"icon"`
+		} `json:"weather"`
+		Wind Wind `json:"wind"`
+	} `json:"list"`
 }
 
 func (rr *RequestResults) UnmarshalJSON(bs []byte) error {
@@ -188,4 +282,47 @@ func (rr *RegionResult) UnmarshalJSON(bs []byte) error {
 	}
 
 	return nil
+}
+
+var Icons = map[string]string{
+	"01d": "☀️",
+	"01n": "🌙",
+	"02d": "🌤 ️",
+	"02n": "🌙",
+	"03d": "🌥 ",
+	"03n": "☁️",
+	"04d": "☁️",
+	"04n": "☁️",
+	"09d": "🌧 ️",
+	"09n": "🌧 ️",
+	"10d": "🌦 ",
+	"10n": "🌧 ️",
+	"11d": "🌩 ",
+	"11n": "🌩 ",
+	"13d": "❄️",
+	"13n": "❄️",
+	"50d": "🌫 ",
+	"50n": "🌫 ",
+}
+
+func GetWindIcon(w *Wind) string {
+	direction := "⬇"
+	switch {
+	case w.Deg >= 22.5 && w.Deg < 67.5:
+		direction = "↙"
+	case w.Deg >= 67.5 && w.Deg < 112.5:
+		direction = "️⬅️"
+	case w.Deg >= 112.5 && w.Deg < 157.5:
+		direction = "️↖"
+	case w.Deg >= 157.5 && w.Deg < 202.5:
+		direction = "⬆"
+	case w.Deg >= 202.5 && w.Deg < 247.5:
+		direction = "↗"
+	case w.Deg >= 247.5 && w.Deg < 292.5:
+		direction = "➡️"
+	case w.Deg >= 292.5 && w.Deg < 337.5:
+		direction = "↘️"
+	}
+
+	return fmt.Sprintf("%s %.fм/с", direction, w.Speed)
 }
